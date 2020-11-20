813afd42612e1e5def0417fa0f2645e526a61190
dmytro.bezkorovainyi <dmytro.bezkorovainyi@indeema.com>
2020-11-20 15:11:13 +0200

    add funktions

--- Study_of_C/Study_class/main.cpp
+++ Study_of_C/Study_class/main.cpp
@@ -2,10 +2,15 @@
 
 class Human
 {
+	
 public:
 	int age;
 	string name;
 
+	void print()
+	{
+		cout << name << " " << age << endl;
+	}
 private:
 
 };
@@ -15,8 +20,7 @@ int main()
 	Human fistHuman;
 	fistHuman.age = 2;
 	fistHuman.name = "Vasily";
-	cout << fistHuman.age << endl;
-	cout << fistHuman.name << endl;
+	fistHuman.print();
 
 	_getch();
 	return 0;
